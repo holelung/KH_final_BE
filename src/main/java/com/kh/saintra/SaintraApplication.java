b9c606c92fa819e351dde543f173e4cdaf1e7bdf
holelung <junho335276@gmail.com>
2025-06-10 16:34:38 +0900

    first commit

---
+++ src/main/java/com/kh/saintra/SaintraApplication.java
@@ -0,0 +1,13 @@
+package com.kh.saintra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/saintra/SaintraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.saintra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
