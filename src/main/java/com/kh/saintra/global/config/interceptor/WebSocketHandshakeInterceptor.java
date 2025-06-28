a0be2d5114bec947c500398cfbb02d218fadc31d
holelung <junho335276@gmail.com>
2025-06-28 16:08:32 +0900

    STOMP 설정중..

---
+++ src/main/java/com/kh/saintra/global/config/interceptor/WebSocketHandshakeInterceptor.java
@@ -0,0 +1,35 @@
+package com.kh.saintra.global.config.interceptor;
+
+import java.util.Map;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+@Component
+public class WebSocketHandshakeInterceptor implements HandshakeInterceptor{
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+            WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null) {
+            attributes.put("authentication", authentication); // WebSocket 세션에 저장
+        }
+        
+        return true; // 핸드셰이크 허용
+        
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+            WebSocketHandler wsHandler, Exception exception) {
+       // 아무것도 안함
+    }
+    
+}
